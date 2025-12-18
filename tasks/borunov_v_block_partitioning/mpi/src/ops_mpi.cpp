83d0fea1a77538540be5e7cd77ec6ed65ca378b4
vlad3005 <vladborunov396@gmail.com>
2025-12-18 18:36:36 +0300

    task3 static fix 5

--- tasks/borunov_v_block_partitioning/mpi/src/ops_mpi.cpp
+++ tasks/borunov_v_block_partitioning/mpi/src/ops_mpi.cpp
@@ -102,20 +102,27 @@ bool BorunovVBlockPartitioningMPI::RunImpl() {  // NOLINT(readability-function-c
   for (int gi = row_start; gi < row_end; ++gi) {
     const int local_i = gi - row_start;
     for (int j = 0; j < width; ++j) {
+      const int x0 = std::clamp(j - 1, 0, width - 1);
+      const int x1 = j;
+      const int x2 = std::clamp(j + 1, 0, width - 1);
+
+      const int y0 = std::clamp(gi - 1, 0, height - 1);
+      const int y1 = gi;
+      const int y2 = std::clamp(gi + 1, 0, height - 1);
+
       float sum = 0.0F;
 
-      for (int ky = -1; ky <= 1; ++ky) {
-        for (int kx = -1; kx <= 1; ++kx) {
-          const int nx = std::clamp(j + kx, 0, width - 1);
-          const int gy = std::clamp(gi + ky, 0, height - 1);
-          const int val = pixels[(gy * width) + nx];
+      sum += static_cast<float>(pixels[(y0 * width) + x0]) * kernel[0][0];
+      sum += static_cast<float>(pixels[(y0 * width) + x1]) * kernel[0][1];
+      sum += static_cast<float>(pixels[(y0 * width) + x2]) * kernel[0][2];
 
-          sum += static_cast<float>(val) *
-                 kernel[static_cast<std::size_t>(ky + 1)][static_cast<std::size_t>(
-                     kx +
-                     1)];  // NOLINT(cppcoreguidelines-pro-bounds-constant-array-index,bugprone-misplaced-widening-cast)
-        }
-      }
+      sum += static_cast<float>(pixels[(y1 * width) + x0]) * kernel[1][0];
+      sum += static_cast<float>(pixels[(y1 * width) + x1]) * kernel[1][1];
+      sum += static_cast<float>(pixels[(y1 * width) + x2]) * kernel[1][2];
+
+      sum += static_cast<float>(pixels[(y2 * width) + x0]) * kernel[2][0];
+      sum += static_cast<float>(pixels[(y2 * width) + x1]) * kernel[2][1];
+      sum += static_cast<float>(pixels[(y2 * width) + x2]) * kernel[2][2];
 
       local_res[(local_i * width) + j] = static_cast<int>(std::round(sum));
     }
